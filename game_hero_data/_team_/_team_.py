1b9848d302f12882cfaab15eb5f4c0e5b486bf08
zhengtingeing <1092419259@qq.com>
2017-06-14 21:49:18 +0800

    Create _team_.py

--- game_hero_data/_team_/_team_.py
+++ game_hero_data/_team_/_team_.py
@@ -4,4 +4,4 @@ with open('_team_.tsv', encoding='utf8') as f:
     reader = csv.DictReader(f)
     print ('\t'.join(reader.fieldnames))
     for r in reader:
-        print ('\t'.join([r[k] for k in reader.fieldnames]) )
+        print ('\t'.join([r[x] for x in reader.fieldnames]) )
